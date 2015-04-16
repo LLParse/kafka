64e1eeabcda135e94cc834870bdb434de6ca60e3
James Oliver <james.oliver@stealth.ly>
2015-04-16 00:17:49 -0700

    cleanup

--- docker-cli.sh
+++ docker-cli.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 jar='kafka-mesos*.jar'
 
 check_jar() {
@@ -16,14 +32,6 @@ check_jar() {
 }
 
 check_environment() {
-	# DEBUG=true
-	# CLUSTER_STORAGE=file:kafka-mesos.json
-	# MESOS_USER=
-	# MASTER_CONNECT=zk://localhost:2181/mesos
-	# KAFKA_ZK_CONNECT=localhost:2181
-	# SCHEDULER_URL=http://localhost:7000
-	# FAILOVER_TIMEOUT=36000
-
 	if [ ! -z "$DEBUG" ]; then
 		sed -r -i "s|(debug=).*|\1$DEBUG|g" kafka-mesos.properties
 	fi
@@ -50,4 +58,3 @@ check_environment() {
 check_jar
 check_environment
 java -jar $jar "$@"
-
